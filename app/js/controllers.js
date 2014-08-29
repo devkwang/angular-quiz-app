fb975c5fb07c2b6e1d10950c79a37cf3f0d3b3ef
devkwang <devkwang@gmail.com>
2014-08-29 17:07:49 -0400

    Update controllers.js
    changed to absolute address

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,7 @@
 function QuestionsController($scope, $http, $routeParams, $location, $resultsService) {
 
   $scope.questionNo = $routeParams && $routeParams.questionNo ? $routeParams.questionNo : 1;
-  $http.get('/questions/questions.json').success(function (questions){
+  $http.get('http://devkwang.github.io/angular-quiz-app/app/questions/questions.json').success(function (questions){
     $scope.questions = questions;
   });
 
